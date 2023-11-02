5c497c7653b18f3e2e1573bd8f2fc68e1193efc3
gr421shpean <149349454+gr421shpean@users.noreply.github.com>
2023-11-02 09:18:38 +0700

    Миграция и изменеие порта

--- design/design_studio/apps.py
+++ design/design_studio/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DesignstudioConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'designstudio'
+    name = 'design_studio'
