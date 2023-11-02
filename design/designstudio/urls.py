f66914b037f85c1983ec430fe45a82921679b8fe
gr421shpean <149349454+gr421shpean@users.noreply.github.com>
2023-11-02 21:17:00 +0700

    Изменение base.html и добавление путей

--- design/designstudio/urls.py
+++ design/designstudio/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.base, name='base'),
     path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
 ]
